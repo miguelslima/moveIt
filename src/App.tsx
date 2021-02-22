80694056cf41d5597dc1f3fd8e637f95a199699d
Miguel <miguelsousalima@hotmail.com>
2021-02-22 16:43:32 -0300

    Settings Components Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button } from "./components/Button";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button color="red">Teste</Button>
+      <Button color="blue">Teste</Button>
+      <Button color="green">Teste</Button>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+interface ButtonProps {
+  color: string;
+  children: string;
+}
+
+export function Button(props: ButtonProps) {
+  const [counter, setCounter] = useState(1);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <button
+      onClick={increment}
+      type="button"
+      style={{ backgroundColor: props.color }}
+    >
+      {props.children} <strong>{counter}</strong>
+    </button>
+  );
+}
